86b2cdc8197ea299bf00b5bf9f5a5f124e84b451
王慧龙 <wanghuilongsd@163.com>
2018-02-25 17:03:23 +0800

    添加项目文件。

---
+++ DesktopVideo/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopVideo
+{
+    public partial class Form1 : Form
+    {
+        public Socket sockClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sockClient = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAddress = new IPAddress(new byte[] { 127, 0, 0, 1 });
+            EndPoint point = new IPEndPoint(ipAddress, 1997);
+            sockClient.Connect(point);
+            byte[] data = new byte[1024];
+            int leng=sockClient.Receive(data);
+            MessageBox.Show(Encoding.UTF8.GetString(data, 0, leng));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sockClient.Send(System.Text.Encoding.UTF8.GetBytes(textBox1.Text));
+        }
+    }
+}
